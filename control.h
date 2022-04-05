640cad11ae01c72a07481767212096a765f840c8
Vinícius <viniciussmmedeiros@gmail.com>
2022-04-05 12:08:13 -0300

    atualizando funcoes de controle

--- control.h
+++ control.h
@@ -6,6 +6,10 @@ void push(vector<int>& pilha, int elemento) {
     pilha.push_back(elemento);
 }
 
+int pushr(vector<int>& pilha) {
+    return pilha[pilha.size() - 1];
+}
+
 void pop(vector<int> pilha) {
     pilha.pop_back();
 }
