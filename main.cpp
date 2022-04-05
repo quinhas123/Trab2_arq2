4ed35b20254391b0acfa7a5f116ff4f26bd15031
Vinícius <viniciussmmedeiros@gmail.com>
2022-04-05 01:05:02 -0300

    adicionando leitura de labels e jumps

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>    // para uso do find, encontrar string específica no vector
 #include "arithmetic.h"                                     // importações dos arquivos externos '.h' contendo as funções criadas
 #include "control.h"
 
@@ -11,29 +12,85 @@ int main() {
     cout << "########################################################";
     cout << "\n\t\tSIMULADOR MAQUINA DE PILHA";
     cout << "\n########################################################";
+
 // ####################### Para fins de teste #######################
-    vector<int> stack;
+    vector<int> pilha;
     int registrador;
 // ##################################################################
 
     std::ifstream assemblyCode;
-    assemblyCode.open("teste-arq.txt");                     // Mudar o nome para o arquivo desejado
+    assemblyCode.open("n_numprimo.txt");                     // Mudar o nome para o arquivo desejado
 
-    int lineCounter = 0;                                    // Para identificar a linha de erro
+    vector<string> labels;
+    vector<int> labelsLinhas;
 
     if(assemblyCode.is_open()) {
         string codeLine;
+        bool flagJump = false;                                  // flag para pular linhas quando procurando pela linha de um label específico
+        int linhaLabel;                                         // indica a linha do label a ser procurado após um jump
+        int lineCounter = 0;                                    // variável para apontar para a linha atual do arquivo
 
+        // percorre todo o arquivo para salvar os labels e suas respectivas linhas
+        while(getline(assemblyCode, codeLine)) {
+            string instrucao;
+            string operando = "\0";
+            bool ehinstrucao = true;
+            int i = 0;
+
+            lineCounter++;
+
+            while(codeLine[i] == ' ' || codeLine[i] == '\t'){	// Ignora espaços no começo da linha
+                i++;
+            }
+
+            if(codeLine[i] == ';') {                            // Se a linha já começar com comentário será ignorada
+                continue;
+            }
+
+            while(i < codeLine.size() && codeLine[i] != ';'){   // Percorre cada um dos caracteres da linha atual
+                if(codeLine[i] == ' '){                         // Quando encontrar espaço passa a concatenar os caracteres a 'operando'
+                    ehinstrucao = false;
+                }
+                else if(ehinstrucao){
+                    instrucao += codeLine[i];
+                }
+                else{
+                    operando += codeLine[i];
+                }
+
+                i++;
+            }
+
+            if(instrucao.back() == ':') {
+                instrucao.pop_back();                       // remove os : do label
+                labels.push_back(instrucao);                // adiciona o label ao vetor de labels
+                labelsLinhas.push_back(lineCounter + 1);     // adiciona a linha + 1 do label ao vetor de linhas de labels. + 1 para pular para a região logo após label
+            }
+        }
+
+        lineCounter = 0;
+
+        // limpa as flags do arquivo para percorre-lo novamente e agora compilar o código
+        assemblyCode.clear();
+        assemblyCode.seekg(0);
+        
         while(getline(assemblyCode, codeLine)) {            // Laço para ir 'pegando' as linhas do arquivo
             string instrucao;
             string operando = "\0";
             bool ehinstrucao = true;                        // Ajuda a identificar quando deve concatenar os caracteres do arquivo a 'instrucao' ou a 'operando'
             bool temOperando;                               // Para gerar erro sobre comandos que deveriam ou não possuir operando
             bool operandoEhRegistrador;                     // Flag para identificar se o operando passado foi um número ou o próprio registrador. Ex: PUSH 8 ou PUSH $R
+            bool operandoEhLabel = false;
             int numOperando;                                // Guarda o valor do operando após ele ser convertido de string para int
             int i = 0;
 
             lineCounter++;
+
+            if(flagJump) {
+                if(lineCounter != linhaLabel) {             // se a flag de jump estiver ligada, percorre todas as linhas até chegar na desejada
+                    continue;
+                }
+            }
             
             while(codeLine[i] == ' ' || codeLine[i] == '\t'){	// Ignora espaços no começo da linha
                 i++;
@@ -73,68 +130,112 @@ int main() {
             if(temOperando) {
                 if(operando == "$R") {                      // Atualiza a flag do tipo do operando
                     operandoEhRegistrador = true;
+                }else if(find(labels.begin(), labels.end(), operando) != labels.end()){
+                    operandoEhLabel = true;
                 }else {
                     operandoEhRegistrador = false;
                 }
 
                 if(operandoEhRegistrador) {
                     numOperando = registrador;              // Se operando == $R, então o valor de $R é passado para numOperando
-                }else {
+                }else if(!operandoEhLabel) {
                     numOperando = stoi(operando);           // Se operando tiver sido algum número, converte de string para int (stoi)
                 }
             }
 
+            if(instrucao.back() == ':') {
+                continue;
+            }
+
+            if(instrucao == "JMP" || instrucao == "IF0") {
+                int indiceLabel;
+
+                for(int i = 0; i < labels.size(); i++) {
+                    if(labels[i] == operando) {
+                        indiceLabel = i;
+                    }
+                }
+
+                linhaLabel = labelsLinhas[indiceLabel];
+                lineCounter = 0;
+                flagJump = true;
+
+                assemblyCode.clear();
+                assemblyCode.seekg(0);
+                continue;
+            }else {
+                flagJump = false;
+            }
+
             if(instrucao == "PUSH" && !temOperando) {       // Dispara erro caso PUSH tenha sido passada sem operando
-                cout << "[000] - Erro de sintaxe. Eh esperado que as instrucoes possuam zero (POP) ou um operandos (PUSH $R). -> [linha " << lineCounter << "]";
+                cout << "\n[000] - Erro de sintaxe. Eh esperado que as instrucoes possuam zero (POP) ou um operandos (PUSH $R). -> [linha " << lineCounter << "]";
                 break;
-            }else if(instrucao == "POP" && temOperando) {   // Dispara erro caso POP tenha sido passada com algum operando
-                cout << "[000] - Erro de sintaxe. Eh esperado que as instrucoes possuam zero (POP) ou um operandos (PUSH $R). -> [linha " << lineCounter << "]";
+            }
+            else if(instrucao == "POP" && temOperando) {   // Dispara erro caso POP tenha sido passada com algum operando
+                cout << "\n[000] - Erro de sintaxe. Eh esperado que as instrucoes possuam zero (POP) ou um operandos (PUSH $R). -> [linha " << lineCounter << "]";
                 break;         
-            }else if((instrucao == "CLEAR" || instrucao == "ADD" || instrucao == "SUB" || instrucao == "MUL" || instrucao == "DIV" || instrucao == "MOD") && temOperando) {
+            }
+            else if((instrucao == "CLEAR" || instrucao == "ADD" || instrucao == "SUB" || instrucao == "MUL" || instrucao == "DIV" || instrucao == "MOD") && temOperando) {
                 // Dispara erro caso alguma das instruções aritméticas tenham sido passadas com operando
-                cout << "[002] - Argumento inválido -> [Linha " << lineCounter << "]";
+                cout << "\n[002] - Argumento inválido -> [Linha " << lineCounter << "]";
                 break;
             }
 
             if(instrucao == "CLEAR") {
 
-            }else if(instrucao == "ADD") {
-                registrador = add(stack, stack.size() - 1);
-            }else if(instrucao == "SUB") {
+            }
+            else if(instrucao == "ADD") {
+                registrador = add(pilha, pilha.size() - 1);
+            }
+            else if(instrucao == "SUB") {
 
-            }else if(instrucao == "MUL") {
+            }
+            else if(instrucao == "MUL") {
 
-            }else if(instrucao == "DIV") {
+            }
+            else if(instrucao == "DIV") {
 
-            }else if(instrucao == "MOD") {
+            }
+            else if(instrucao == "MOD") {
 
-            }else if(instrucao == "NOT") {
+            }
+            else if(instrucao == "NOT") {
 
-            }else if(instrucao == "OR") {
+            }
+            else if(instrucao == "OR") {
                 
-            }else if(instrucao == "AND") {
+            }
+            else if(instrucao == "AND") {
 
-            }else if(instrucao == "MIR") {
+            }
+            else if(instrucao == "MIR") {
 
-            }else if(instrucao == "PUSH") {
-                if(stack.size() == 128) {
-                    cout << "[004] - PUSH em stack cheia -> [Linha " << lineCounter << "]";
+            }
+            else if(instrucao == "PUSH") {
+                if(pilha.size() == 128) {
+                    cout << "\n" << pilha.size();
+                    cout << "\n[004] - PUSH em pilha cheia -> [Linha " << lineCounter << "]";
                     break; 
                 }else {
-                    push(stack, numOperando);
+                    push(pilha, numOperando);
                 }
-            }else if(instrucao == "POP") {
-                if(stack.size() == 0) {
-                    cout << "[003] - POP em stack vazia -> [Linha " << lineCounter << "]";
+            }
+            else if(instrucao == "POP") {
+                if(pilha.size() == 0) {
+                    cout << "[003] - POP em pilha vazia -> [Linha " << lineCounter << "]";
                     break; 
                 }else {
-                    pop(stack);
+                    pop(pilha);
                 }
-            }else if(instrucao == "OUT") {
-                cout << "\n\nTopo da pilha: " << stack[stack.size() - 1];
+            }
+            else if(instrucao == "OUT") {
+                cout << "\n\nTopo da pilha: " << pilha[pilha.size() - 1];
                 cout << "\n\nFim do programa.";
-            }else {
-                cout << "[001] - Instrucao invalida. Ex. POOP em vez de POP. -> [Linha " << lineCounter << "]";
+            }
+            else {
+                // cout << "\n\n" << instrucao;
+                // cout << "\n\n" << instrucao.length();
+                cout << "\n[001] - Instrucao invalida. Ex. POOP em vez de POP. -> [Linha " << lineCounter << "]";
                 break; 
             }
         }
