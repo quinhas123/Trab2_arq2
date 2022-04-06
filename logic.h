985b7d87c7b3651645a90513bd16b24454fcf2d4
Vinícius <viniciussmmedeiros@gmail.com>
2022-04-06 18:07:52 -0300

    adicionando funcoes de add e or

--- logic.h
+++ logic.h
@@ -6,6 +6,14 @@ void notLogic(vector<int16_t>& pilha){
     pilha[pilha.size() - 1] = ~pilha[pilha.size() - 1];
 }
 
+int16_t andLogic(vector<int16_t>& pilha, int16_t registrador){
+    return pilha[pilha.size() - 1] & registrador;
+}
+
+int16_t orLogic(vector<int16_t>& pilha, int16_t registrador){
+    return pilha[pilha.size() - 1] | registrador;
+}
+
 void mir(vector<int16_t>& pilha){
     unsigned int num = pilha[pilha.size() - 1];
     unsigned int numRev = 0;
