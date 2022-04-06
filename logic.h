7f2a8f14a907215bca6d708bf776dc623f1ee470
Vinícius <viniciussmmedeiros@gmail.com>
2022-04-06 12:51:21 -0300

    atualizando funcoes de operações lógicas

--- logic.h
+++ logic.h
@@ -5,3 +5,20 @@ using namespace std;
 void notLogic(vector<int>& pilha){
     pilha[pilha.size() - 1] = ~pilha[pilha.size() - 1];
 }
+
+void mir(vector<int>& pilha){
+    int num = pilha[pilha.size() - 1];
+    int pos = sizeof(num) - 1;
+    int reverse = 0;
+
+    while(pos >= 0 && num) {
+        if(num & 1) {
+            reverse = reverse | (1 << pos);
+        }
+
+        num >>= 1;
+        pos--;
+    }
+
+    pilha[pilha.size() - 1] = reverse;
+}
